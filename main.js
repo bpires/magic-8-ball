be6461259bdf89b438fd4c632e17790ca724bea3
Rafael Barros <86871991+bpires@users.noreply.github.com>
2022-06-12 20:30:17 -0300

    style input

--- main.js
+++ main.js
@@ -3,26 +3,26 @@ const inputQuestion = document.querySelector("#inputQuestion")
 const askButton = document.querySelector('#askButton')
 const triangle = document.querySelector('#triangle')
 const answers = [
-    "It is certain.",
-    "It is decidedly so.",
-    "Without a doubt.",
-    "Yes definitely.",
-    "You may rely on it.",
-    "As I see it, yes.",
-    "Most likely.",
-    "Outlook good.",
-    "Yes.",
-    "Signs point to yes.",
-    "Reply hazy, try again.",
-    "Ask again later.",
-    "Better not tell you now.",
-    "Cannot predict now.",
-    "Concentrate and ask again.",
-    "Don't count on it.",
-    "My reply is no.",
-    "My sources say no.",
-    "Outlook not so good.",
-    "Very doubtful.",
+    "It is certain",
+    "It is decidedly so",
+    "Without a doubt",
+    "Yes, definitely",
+    "You may rely on it",
+    "As I see it, yes",
+    "Most likely",
+    "Outlook good",
+    "Yes",
+    "Signs point to yes",
+    "Reply hazy, try again",
+    "Ask again later",
+    "Better not tell you now",
+    "Cannot predict now",
+    "Concentrate and ask again",
+    "Don't count on it",
+    "My reply is no",
+    "My sources say no",
+    "Outlook not so good",
+    "Very doubtful",
 ]
 
 function ask() {
@@ -49,5 +49,6 @@ function ask() {
     inputQuestion.classList.remove('hide')
     triangle.classList.remove('show')
     answer.classList.remove('show')
+    inputQuestion.value = ""
   }, 2000)
 }
